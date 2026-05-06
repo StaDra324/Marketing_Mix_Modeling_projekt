9c3b337bbd7fcf6b2bfd08be5143c2ecd02998f3
StaDra324 <s.drag@student.uw.edu.pl>
2026-05-06 19:27:51 +0200

    W trakcie walidacji modelu
    Walidacja biznesowa zakończona pomyślnie, w trakcie dobierania interakcji dummies do unormowania reszt

--- kod_mmm.R
+++ kod_mmm.R
@@ -721,11 +721,9 @@ model <- lm(data = data.df,
 
 # - makroekonomia nieistotna
 
-# - ostatecznie uzyskany model osiągnął R^2 91,7%, skorygowane R^2 90,6%,
-#     nie występują w nim współliniowość
+#### WALIDACJA MODELU ####
 
-
-# Model docelowy
+# Specyfikacja po modelowaniu
 model <- lm(data = data.df,
             I(VO_B02 / mean(VO_B02)) ~
               I(TI_SEASONALITY / mean(TI_SEASONALITY)) +
@@ -741,221 +739,198 @@ model <- lm(data = data.df,
               I(EX_NU_B02 / mean(EX_NU_B02)) +
               TV50_B02_2011_H1)
 
-
 summary(model)
 vif(model)
-jarque.bera.test(model$residuals)
-bptest(model)
-bgtest(model)
 
+# Postać i podstawowe statystyki modelu
 
+# Uzyskany model osiągnął R^2 91,7%, skorygowane R^2 90,6%, brak współlinio-
+#   wości. Zmienną objaśnianą był wolumen sprzedaży marki jako indeks (podzie-
+#   lony przez średnią), a jako zmienne objaśniające uwzględnione zostały:
+#   sezonowość (indeks), dystrybucje (jako inkrementalność) in-outów
+#   B02_S02_12XCN0500_2GR, B02_S02_08XCN0500_1GR i B02_S02_12XCN0500, logarytm
+#   ceny stałego SKU B02_S02_04XCN0500, majówka, Niedziela Wielkanocna, ty-
+#   dzień przed Halloween, Zielone Świątki, odchylenie temperatury od normy,
+#   ekspozycja numeryczna marki (indeks) oraz intensywność reklamy w TV w 
+#   pierwszym półroczu 2011 z AdStockiem na poziomie 50%.
 
+# Walidacja biznesowa
 
+# Wszystkie parametry mają sens biznesowy, zarówno pod kątem znaku jak i wiel-
+#   kości, z wyjątkiem ceny, dla której skorygowana elastyczność wynosi ok. 
+#   -7,6%, jednak jest to odchylenie na tyle małe, że jest akceptowalne, a poza
+#   tym może wynikać z danych
 
-# Model +/- droga od początku 
-model <- lm(data = data.df,
-           I(VO_B02 / mean(VO_B02)) ~
-             I(TI_SEASONALITY / mean(TI_SEASONALITY)) +
-             DN_adj_B02_S02_12XCN0500_2GR +
-             DN_adj_B02_S02_08XCN0500_1GR +
-             DN_adj_B02_S02_12XCN0500 +
-             log(PR_B02_S02_04XCN0500) +  
-             #log(PR_B02_S02_01XCN0500) +  
-             #log(PR_B02_S02_01XNR0660) +
-             #DN_adj_B02_S02_01XNR0660 +
-             #DN_adj_B02_S01_04XCN0500 +
-             #DN_B02 +
-             #NS +
-             #TI_H_NEW_YEAR +
-             TI_H_MAY +
-             #TI_H_XMAS +
-             #TI_H_XMAS_BEFORE +
-             #TI_H_XMAS_BEFORE2 +
-             #TI_H_ASSUM_OF_MARY +
-             #TI_H_CORPUS_CHRISTI +
-             #TI_H_EASTER_MONDAY +
-             TI_H_EASTER_SUNDAY +
-             #TI_H_EASTER_SUNDAY_BEFORE +
-             #TI_H_EPIPHANY +
-             #TI_H_HALLOWEEN +
-             TI_H_HALLOWEEN_BEFORE +
-             #TI_H_INDEPENDENCE +  ewentualnie do dodania
-             TI_H_PENTECOST +
-             # święta ewentualnie do sprawdzenia na łączną nieistotność
-             I(TI_TEM_AVG - TI_TEM_AVG_NORM) +
-             I(EX_NU_B02 / mean(EX_NU_B02))
-             #DN_B01_S01_12XCN0500_2GR
-             #I(EX_NU_B01 / mean(EX_NU_B01))
-             #EV_FOOTBALL_WORLD_CUP +
-             #EV_WINTER_OLYMPIC +
-             #TV00_B02 +
-             #TV90_B02 +
-             #TV50_B02 +
-             #TV50_B02_2010_H2 +
-             #TV50_B02_2011_H2 +
-             #TV50_B02_2010_H1 +
-             TV50_B02_2011_H1
-             #OH00_B02 +
-             #OH90_B02 +
-             #OH90_B02_2011_H2 +         
-             #OH20_B02 +
-             #RA00_B02 +
-             #TV50_B01 +
-             #EC_CPI_MA5 +
-             #EC_PCE_MA5 +
-             #EC_UNE_MA5 +
-             
-  )
+# Walidacja statystyczna
 
-summary(model)
+# Reszty
 
 jarque.bera.test(model$residuals)
-vif(model)
-bptest(model)
-bgtest(model)
-# Co się działo w modelu podczas dodawania kolejnych zmiennych:
-#   - sama sezonowość ma nieduży wpływ, ok. 0,4%, R^2 3% i pval 7%
+# brak normalności reszt
 
-#   - inkrementalność DN_B02_S02_12XCN0500_2GR ładnie wchodzi i poprawia,
-#       jest w przedziale <0; 1>,R^2 60%, ale heteroskedastyczność
-
-#   - inkrementalność DN_adj_B02_S02_08XCN0500_1GR nie wchodzi, pval 67%
-#       i zły znak, jako że na wykresie nie było bardzo widoczne to może
-#       być do usunięcia potem, ale się zobaczy
-
-#   - inkrementalność DN_adj_B02_S02_12XCN0500 w miarę wchodzi, ale pval 25%, 
-#       trochę dziwne, że to wchodzi a DN_adj_B02_S02_08XCN0500_1GR nie, ale 
-#       zobaczymy co dalej
-
-#   - log ceny PR_B02_S02_04XCN0500 wchodzi idealnie, R^2 84%, znaki i pval w
-#       w DN_adj_B02_S02_08XCN0500_1GR i DN_adj_B02_S02_12XCN0500 zmieniają
-#       się na poprawne
-
-#   - log ceny PR_B02_S02_01XCN0500 nie wchodzi, pval 70% i zły znak, ale z 
-#       wykresu mógłby wejść, zobaczymy co dalej
-
-#   - log ceny PR_B02_S02_01XNR0660 też nie wchodzi, pval 25% i zły znak, ale z 
-#       wykresu bardziej dystrybucja
-
-#   - ,,inkrementalność'' (stały SKU) PR_B02_S02_01XNR0660 też nie wchodzi, 
-#       pval 50% i znacznie za duża wartość, w wykresu mogłoby wejść, zoba-
-#       czymy co dalej 
-
-#   - dystrybucja marki w miarę wchodzi, dobry znak, pval 11%, do obserwacji
-
-#   - NS nieistotne praktycznie i statystycznie, pval 90%, drobna
-#       współliniowość z PR_B02_S02_04XCN0500, raczej do wyrzucenia
-
-#   - Nowy Rok nie wchodzi, pval 85%, trochę dziwne
-
-#   - Majówka wchodzi (z pval 12%, ale mocno więc do zostawienia)
-
-#   - Boże Narodzenie i przed nie wchodzą, wysokie pval >90%
-
-#   - ze świąt zachowane na teraz w modelu oprócz tych wyżej są Boże Ciało,
-#       Poniedziałek Wielkanocny, Niedziela Wielkanocna, przed Halloween, 
-#       Pentecoste
-
-#   - odchylenie temperatury w miarę wchodzi, dobry znak ale pval 20%
-
-#   - ekspozycja dobrze wchodzi zarówno jako numeryczna jak i udział, w obu 
-#       przypadkach dobry znak, ładnie koryguje pval dla świąt, odchylenia
-#       temperatury i dystrybucji. Na podstawie analizy graficznej zostawiam
-#       wersję numeryczną, i na podstawie jej wpływu na model do usunięcia 
-#       będą DN_B02, Poniedziałek Wielkanocny i Boże Ciało. Ogólnie weszło
-#       na tyle dobrze, że aż sprawdziłem czy nie poprawiło tych zmiennych
-#       które nie wchodziły wcześniej, zwłaszcza cen i dystrybucji, ale nie.
-#       R^2 91,5%
-
-#   - SKU konkurecji DN_B01_S01_12XCN0500_2GR nie wchodzi, dobry znak ale pval
-#       76%, jako że ogólnie SKU konkurencji pomijam to wyrzucam z modelu
-
-#   - ekspozycja konkurencji EX_NU_B01 ma pval 21% i i tak bardzo mały para-
-#       metr, wyrzucam jak wyżej
-
-#   - Mundial nie wchodzi, dobry znak ale pval 50%
-
-#   - Igrzyska Zimowe też nie wchodzą, zły znak i pval 90%
-
-#   - całe TV nie wchodzi, pval bardzo wysokie i parametr praktycznie 0,
-#     z rozbitych na półrocza wszystkie oprócz pierwszego półrocza 2011
-#     TV50_B02_2011_H1 też, a ono też ma pval 15%, więc biorąc pod uwagę
-#     wykresy może być do wyrzucenia
-
-# - przy outdorze jest ciekawa, a w zasadzie to nieciekawa sytuacja, bo
-#     wraz ze wzrostem AdStocku zmniejsza się pval, do tego stopnia, że
-#     przy OH00 jest 93%, przy OH70 34%, a przy oH90 10%, więc pod kątem
-#     pval można by już rozważyć zostawienie. Tylko że jest zły znak, bo 
-#     jest ujemny, a spodziewać by się można raczej, że wzrost wydatków
-#     będzie przekładał się na wzrost sprzedaży. Dodatkowo wprowadzenie
-#     OH90 poprawia zachowanie TV50_B02_2011_H1. Rozbicie OH90 nie pomaga,
-#     jeśli znak się nie zmieni to trzeba będzie wyrzucić
-
-# - radio trochę podobnie i odwrotnie jak outdoor. Ogólnie do AdStocka 60%
-#     ma niskie pval i robi OH90 nieistotne, ale ma zły znak (ujemny). Z
-#     drugiej strony na AdStocku 0% zmienia znak OH20 na poprawny, ale pval
-#     jest 33%, a z kolei zwiększa się pval na TV50_B02_2011_H1. Więc z 
-#     jednej strony można by zostawić radio bo poprawia OH, ale to by było 
-#     na siłę według mnie, więc jeśli makro czegoś bardzo nie zmieni to z 
-#     mediów zostawiłbym tylko TV50_B02_2011_H1 
-
-# - TV konkurencji nie wchodzi, pval 67%
-
-# - makroekonomia nieistotna
-
-
-p <- plot_ly(data.df, 
-             type = "scatter", 
-             mode = 'lines',
-             x = ~Date, 
-             y = ~TI_TEM_AVG,
-             name = "TI_TEM_AVG") %>%
-  add_trace(
-    mode = 'lines',
-    x = ~Date, 
-    y = ~TI_TEM_AVG_NORM, 
-    name = "TI_TEM_AVG_NORM")
+# Wykres reszt
+p <-  plot_ly(data.df,
+              type = "scatter",
+              mode = 'lines',
+              x = ~Date,
+              y = ~model$residuals,
+              name = "Residuals")
 htmlwidgets::saveWidget(p, "wykres.html", selfcontained = TRUE)
 browseURL("wykres.html")
+# widać kilka większych outlierów. Wymieniam od największych: 02.05.2011,
+#   28.03.2011, 03.10.2011, 27.06.2011, 29.08.2011, 05.04.2010
 
+# Kandydatem na 02.05.2011 jest B02_S02_12XCN0500_2GR, jest wtedy w trakcie
+#   piku (widać na wykresie z cenami, wolumenami i dystrybucją z analizy graf-
+#   icznej). Można tam też zobaczyć, że pewnie chodzi o cenę
 
-
-plot_ly(data.df, 
-        type = "scatter", 
+# Wykres wolumenu i ceny B02_S02_12XCN0500_2GR
+p <-  plot_ly(data.df,
+        type = "scatter",
         mode = 'lines',
-        x = ~Date, 
-        y = ~EC_CPI) %>%
-  layout(yaxis = list(range = c(0, max(data.df$EC_CPI))))
-
-
-
-p <- plot_ly(data.df,
-             type = "scatter",
-             mode = 'lines',
-             x = ~Date,
-             y = ~VO_B05_S04_04XCN0500,
-             name = "VO_B05_S04_04XCN0500") %>%
+        x = ~Date,
+        y = ~VO_B02_S02_12XCN0500_2GR,
+        name = "VO_B02_S02_12XCN0500_2GR") %>%
   add_trace(
     mode = 'lines',
     yaxis = "y2",
     x = ~Date,
-    y = ~PR_B05_S04_04XCN0500,
-    name = "PR_B05_S04_04XCN0500") %>%
-  layout(yaxis  = list(range = c(0, max(data.df$VO_B05_S04_04XCN0500))),
+    y = ~PR_B02_S02_12XCN0500_2GR,
+    name = "PR_B02_S02_12XCN0500_2GR") %>%
+  layout(yaxis  = list(range = c(0, max(data.df$VO_B02_S02_12XCN0500_2GR))),
          yaxis2 = list(overlaying = "y",
                        side = "right"))
 htmlwidgets::saveWidget(p, "wykres.html", selfcontained = TRUE)
 browseURL("wykres.html")
+# widać, że faktycznie była wtedy mocna obniżka ceny
+
+# Wstawiamy wzmocnienie działania
+
+data.df <- data.df %>%
+  mutate(TI_X_2011_05_02 = as.integer(Date == as.Date("2011-05-02")))
+
+model <- lm(data = data.df,
+            I(VO_B02 / mean(VO_B02)) ~
+              I(TI_SEASONALITY / mean(TI_SEASONALITY)) +
+              DN_adj_B02_S02_12XCN0500_2GR +
+              DN_adj_B02_S02_08XCN0500_1GR +
+              DN_adj_B02_S02_12XCN0500 +
+              log(PR_B02_S02_04XCN0500) +  
+              TI_H_MAY +
+              TI_H_EASTER_SUNDAY +
+              TI_H_HALLOWEEN_BEFORE +
+              TI_H_PENTECOST +
+              I(TI_TEM_AVG - TI_TEM_AVG_NORM) +
+              I(EX_NU_B02 / mean(EX_NU_B02)) +
+              #TV50_B02_2011_H1 +
+              #log(PR_B02_S02_12XCN0500_2GR)
+              I(TI_X_2011_05_02 * DN_adj_B02_S02_12XCN0500_2GR)
+)
+
+summary(model)
+vif(model)
+
+# - na początku spóbowałem wstawić po prostu cenę w logu bo nie była uwzglę-
+#     dniona w modelu, ale zaburzyła ona inne oszacowania, i wyszła też współ-
+#     liniowość między nią a dystrybucją, więc zamiast tego wstawiam samą in-
+#     terakcję, nie jestem pewien w sumie z czym przemnożyć dzień, bo i dys-
+#     trybucja w obu formach i cena działają, na razie zostawiam cenę i do
+#     doprecyzowania najwyżej. Niezależnie od tego co się wstawi, bardzo ład-
+#     nie wchodzi, jedyne co to podwyższa pval TV50_B02_2011_H1 na 82%, więc
+#     nie ma już za bardzo wyboru i to wyrzucam
+
+jarque.bera.test(model$residuals)
+# normalności reszt bardzo poprawiona, pval 9%, więc zależnie od poziomu
+#   istotności mogłoby nawet przejść, kwestia teraz czy lepiej dalej pop-
+#   rawiać i oddawać stopnie swobody czy to jest wystarczające
+
+# Wykres reszt
+p <-  plot_ly(data.df,
+              type = "scatter",
+              mode = 'lines',
+              x = ~Date,
+              y = ~model$residuals,
+              name = "Residuals")
+htmlwidgets::saveWidget(p, "wykres.html", selfcontained = TRUE)
+browseURL("wykres.html")
+# zostały od największych: 28.03.2011, 03.10.2011, 27.06.2011, 29.08.2011,
+#   05.04.2010
+
+# Kandydata na 28.03.2011 na szybko nie widzę, ale nawinął się na 03.10.2011
+#   a mają podobną wielkość. Kandydatem na 03.10.2011 jest B02_S02_04XCN0500
+#   przez dystrybucję
+
+# Wykres wolumenu i dystrybucji B02_S02_04XCN0500
+p <-  plot_ly(data.df,
+              type = "scatter",
+              mode = 'lines',
+              x = ~Date,
+              y = ~VO_B02_S02_04XCN0500,
+              name = "VO_B02_S02_04XCN0500") %>%
+  add_trace(
+    mode = 'lines',
+    yaxis = "y2",
+    x = ~Date,
+    y = ~DN_B02_S02_04XCN0500,
+    name = "DN_B02_S02_04XCN0500") %>%
+  layout(yaxis  = list(range = c(0, max(data.df$VO_B02_S02_12XCN0500_2GR))),
+         yaxis2 = list(overlaying = "y",
+                       side = "right"))
+htmlwidgets::saveWidget(p, "wykres.html", selfcontained = TRUE)
+browseURL("wykres.html")
+# widać, że faktycznie był wtedy mocny spadek dystrybucji
+
+# Wstawiamy wzmocnienie działania
+
+data.df <- data.df %>%
+  mutate(TI_X_2011_10_03 = as.integer(Date == as.Date("2011-10-03")))
+
+model <- lm(data = data.df,
+            I(VO_B02 / mean(VO_B02)) ~
+              I(TI_SEASONALITY / mean(TI_SEASONALITY)) +
+              DN_adj_B02_S02_12XCN0500_2GR +
+              DN_adj_B02_S02_08XCN0500_1GR +
+              DN_adj_B02_S02_12XCN0500 +
+              log(PR_B02_S02_04XCN0500) +  
+              TI_H_MAY +
+              TI_H_EASTER_SUNDAY +
+              TI_H_HALLOWEEN_BEFORE +
+              TI_H_PENTECOST +
+              I(TI_TEM_AVG - TI_TEM_AVG_NORM) +
+              I(EX_NU_B02 / mean(EX_NU_B02)) +
+              #TV50_B02_2011_H1 +
+              #log(PR_B02_S02_12XCN0500_2GR)
+              I(TI_X_2011_05_02 * DN_adj_B02_S02_12XCN0500_2GR) +
+              I(TI_X_2011_10_03 * DN_B02_S02_04XCN0500)
+)
+
+summary(model)
+vif(model)
+
+# - ładnie wchodzi
+
+jarque.bera.test(model$residuals)
+# normalności reszt znów poprawiona, pval 6%, pytanie jak wcześniej, czy lepiej
+#   poprawiać i oddawać stopnie swobody czy to jest wystarczające
+
+# Wykres reszt
+p <-  plot_ly(data.df,
+              type = "scatter",
+              mode = 'lines',
+              x = ~Date,
+              y = ~model$residuals,
+              name = "Residuals")
+htmlwidgets::saveWidget(p, "wykres.html", selfcontained = TRUE)
+browseURL("wykres.html")
+# zostały od największych: 28.03.2011, 27.06.2011, 29.08.2011, 05.04.2010
 
 
 
 
 
-
-
-
-
-
+reset(model, power = 2:3, type = "fitted")
+bptest(model)
+bgtest(model)
 
 
 
